a069cc9da5797c24732120040dd7a0f8ee006093
Matthew Rada <matthewr7.christopher@gmail.com>
2025-06-16 10:12:43 -0400

    list projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container p-3">
+
+  <div class="container" style="background-color: rgb(255, 255, 255, 0.25)">
+    <h1 class="text-center">Christopher M. Rada</h1>
+  </div>
+
+
+  <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.25)">
+    <h2 class="text-center"><u>Computational Biologist</u></h2>
+
+    <div class="container text-center">
+      <div class="row">
+
+        <div class="col-sm">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              <a href="#" class="btn btn-primary">Learn more</a>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-sm">
+          Column
+        </div>
+        <div class="col-sm">
+          Column
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.25)">
+    <h2 class="text-center"><u>Owner of Radatech LLC</u></h2>
+
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-sm">
+          Column
+        </div>
+        <div class="col-sm">
+          Column
+        </div>
+        <div class="col-sm">
+          Column
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+</div>
+
