4da560dd4d8df493f3cb1a3f00b596ba3885899e
Matthew Rada <matthewr7.christopher@gmail.com>
2025-06-17 12:42:00 -0400

    add imgs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,32 @@
 <div class="container p-3">
 
-  <div class="container" style="background-color: rgb(255, 255, 255, 0.25)">
+  <div class="container" style="background-color: rgb(255, 255, 255, 0.10)">
     <h1 class="text-center">Christopher M. Rada</h1>
   </div>
 
+  <div class="container py-3" style="background-color: rgb(255, 255, 255, 0.10)">
+    <div class="card mx-auto text-center" style="width: 18rem;">
+      <img src="/img/matthew-rada-img.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">About Me</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+        <a href="#" class="btn btn-primary">Linkedln</a>
+      </div>
+    </div>
+  </div>
 
-  <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.25)">
-    <h2 class="text-center"><u>Computational Biologist</u></h2>
 
-    <div class="container text-center">
+  <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.10)">
+    <h2 class="text-center"><u>Bioinformatics Projects</u></h2>
+
+    <div class="container text-center d-flex justify-content-center">
       <div class="row">
 
         <div class="col-sm">
-          <div class="card" style="width: 18rem;">
+          <div class="card mt-2">
             <img src="..." class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Sunlight Right</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
               <a href="#" class="btn btn-primary">Learn more</a>
             </div>
@@ -23,30 +34,58 @@
         </div>
         
         <div class="col-sm">
-          Column
+          <div class="card mt-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">PCRprep</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              <a href="#" class="btn btn-primary">Learn more</a>
+            </div>
+          </div>
         </div>
+
         <div class="col-sm">
-          Column
+          <div class="card mt-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Gene Music Theorem</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              <a href="#" class="btn btn-primary">Learn more</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 
 
-  <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.25)">
+  <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.10)">
     <h2 class="text-center"><u>Owner of Radatech LLC</u></h2>
 
-    <div class="container text-center">
+    <div class="container text-center d-flex justify-content-center">
       <div class="row">
         <div class="col-sm">
-          Column
+          <div class="card mt-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Hope Channel Inc.</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              <a href="#" class="btn btn-primary">Learn more</a>
+            </div>
+          </div>
         </div>
+
         <div class="col-sm">
-          Column
+          <div class="card mt-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Caring Angels LLC</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              <a href="#" class="btn btn-primary">Learn more</a>
+            </div>
+          </div>
         </div>
-        <div class="col-sm">
-          Column
-        </div>
+
       </div>
     </div>
   </div>
