6112139e9b16ffa51a1e3eeaa18267757ebffbcd
Matthew Rada <matthewr7.christopher@gmail.com>
2025-11-05 08:42:51 -0500

    host website next

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,8 @@
       <img src="/img/matthew-rada-img.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">About Me</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" class="btn btn-primary">Linkedln</a>
+        <p class="card-text">Graduate biotech student with an emphasis in bioinformatics and interest in web development.</p>
+        <a href="https://www.linkedin.com/in/christopher-rada-a657a51b6/" target="_blank" class="btn btn-primary">Linkedln</a>
       </div>
     </div>
   </div>
@@ -23,34 +23,35 @@
       <div class="row">
 
         <div class="col-sm">
+
           <div class="card mt-2">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/img/sunlightright.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Sunlight Right</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-              <a href="#" class="btn btn-primary">Learn more</a>
+              <p class="card-text">Project inspired from my experience of chronic sinusitus.</p>
+              <a href="https://sunlightright.org/" target="_blank" class="btn btn-primary">Visit here</a>
             </div>
           </div>
         </div>
         
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/img/pcr-prep-logo.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">PCRprep</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-              <a href="#" class="btn btn-primary">Learn more</a>
+              <p class="card-text">Project designed for sample accessioning and PCR assays.</p>
+              <a href="https://youtu.be/IJtDBgoFGRQ?si=V6MWACA7q3hADEBm" target="_blank" class="btn btn-primary">Watch video</a>
             </div>
           </div>
         </div>
 
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/img/mothur-nf.png" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Gene Music Theorem</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-              <a href="#" class="btn btn-primary">Learn more</a>
+              <h5 class="card-title">Mothur & Nextflow Tutorial Pipeline</h5>
+              <p class="card-text">Miseq SOP Mothur tutorial integrated with Nextflow.</p>
+              <a href="https://github.com/FusRaDa/miseq_sop_nextflow" target="_blank" class="btn btn-primary">View repo</a>
             </div>
           </div>
         </div>
@@ -60,28 +61,29 @@
 
 
   <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.10)">
-    <h2 class="text-center"><u>Owner of Radatech LLC</u></h2>
+    <h2 class="text-center"><u>Radatech LLC</u></h2>
 
     <div class="container text-center d-flex justify-content-center">
       <div class="row">
+
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/img/HC Logo.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Hope Channel Inc.</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-              <a href="#" class="btn btn-primary">Learn more</a>
+              <p class="card-text">Contracter as a web specialist since 2023.</p>
+              <a href="https://hopetv.org/" target="_blank" class="btn btn-primary">Visit here</a>
             </div>
           </div>
         </div>
 
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/img/logo-transparent.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Caring Angels LLC</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-              <a href="#" class="btn btn-primary">Learn more</a>
+              <p class="card-text">Web developer for a caregiving agency.</p>
+              <a href="https://caring-angels-llc.com/" target="_blank" class="btn btn-primary">Visit here</a>
             </div>
           </div>
         </div>
@@ -90,6 +92,27 @@
     </div>
   </div>
 
+
+  <div class="container mt-4 pb-4" style="background-color: rgb(255, 255, 255, 0.10)">
+    <h2 class="text-center"><u>Misc.</u></h2>
+
+    <div class="container text-center d-flex justify-content-center">
+      <div class="row">
+        <div class="col-sm">
+
+          <div class="card mt-2">
+            <img src="/img/robot.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Autoflusher Bot</h5>
+              <p class="card-text">3D printed robot with Arduino hardware built to flush the toilet.</p>
+              <a href="https://youtu.be/WqopIXGkR5I?si=rA0KsFy9vCWZ8E_3" target="_blank" class="btn btn-primary">Watch video</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 
 
 </div>
