fd15d2a4c0660445b7249e2c5299932f45132546
Matthew Rada <matthewr7.christopher@gmail.com>
2026-02-03 10:34:51 -0500

    added rnaseq proj

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
       <img src="/img/matthew-rada-img.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">About Me</h5>
-        <p class="card-text">Graduate biotech student with an emphasis in bioinformatics and experienced in web development.</p>
+        <p class="card-text">Graduate biotech student with pursuing bioinformatics and experience in web development.</p>
         <p class="card-text">Email: matthewr7.christopher@gmail.com</p>
-        <a href="https://www.linkedin.com/in/christopher-rada-a657a51b6/" target="_blank" class="btn btn-primary">Linkedln</a>
+        <a href="https://www.linkedin.com/in/christopher-rada-a657a51b6/" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-brands fa-linkedin"></i></a>
       </div>
     </div>
   </div>
@@ -25,44 +25,53 @@
 
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="/img/Figure_10.png" class="card-img-top" alt="...">
+            <div class="ratio ratio-1x1">
+              <img src="/img/nf-core-rnaseq.PNG" class="card-img-top" alt="...">
+            </div>
             <div class="card-body">
-              <h5 class="card-title">ToxPi Model Weights</h5>
-              <p class="card-text">Reproducing graph results by Fleming et al.</p>
-              <a href="https://github.com/FusRaDa/PMC11175362_reproduce_nf?tab=readme-ov-file" target="_blank" class="btn btn-primary">View repo</a>
+              <h5 class="card-title">NF-Core/RNAseq</h5>
+              <p class="card-text">Running Nextflow's RNAseq pipeline into Google Collab servers.</p>
+              <a href="https://github.com/FusRaDa/RNAseq-nf-core" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-brands fa-github"></i></a>
             </div>
           </div>
         </div>
 
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="/img/mothur-nf.png" class="card-img-top" alt="...">
+            <div class="ratio ratio-1x1">
+              <img src="/img/Figure_10.png" class="card-img-top" alt="...">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ToxPi Model Weights</h5>
+              <p class="card-text">Reproducing graph results by Fleming et al. using Nextflow and R.</p>
+              <a href="https://github.com/FusRaDa/PMC11175362_reproduce_nf?tab=readme-ov-file" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-brands fa-github"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="card mt-2">
+            <div class="ratio ratio-1x1">
+              <img src="/img/mothur-nf.png" class="card-img-top" alt="...">
+            </div>
             <div class="card-body">
               <h5 class="card-title">Mothur & Metagenomics</h5>
               <p class="card-text">Miseq SOP Mothur tutorial integrated with Nextflow.</p>
-              <a href="https://github.com/FusRaDa/miseq_sop_nextflow" target="_blank" class="btn btn-primary">View repo</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm">
-          <div class="card mt-2">
-            <img src="/img/sunlightright.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Sunlight Right</h5>
-              <p class="card-text">Project inspired from my experience of chronic sinusitus.</p>
-              <a href="https://sunlightright.org/" target="_blank" class="btn btn-primary">Visit here</a>
+              <a href="https://github.com/FusRaDa/miseq_sop_nextflow" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-brands fa-github"></i></a>
             </div>
           </div>
         </div>
         
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="/img/pcr-prep-logo.png" class="card-img-top" alt="...">
+            <div class="ratio ratio-1x1">
+              <img src="/img/pcr-prep-logo.png" class="card-img-top" alt="...">
+            </div>
             <div class="card-body">
               <h5 class="card-title">PCRprep</h5>
               <p class="card-text">Project designed for sample accessioning and PCR assays.</p>
-              <a href="https://youtu.be/IJtDBgoFGRQ?si=V6MWACA7q3hADEBm" target="_blank" class="btn btn-primary">Watch video</a>
+              <a href="https://github.com/FusRaDa/pcr-project-2023-global" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-brands fa-github"></i></a>
+              <a href="https://youtu.be/IJtDBgoFGRQ?si=V6MWACA7q3hADEBm" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-youtube-play" id="youtube-icon"></i></a>
             </div>
           </div>
         </div>
@@ -80,22 +89,39 @@
 
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="/img/HC Logo.png" class="card-img-top" alt="...">
+            <div class="ratio ratio-1x1">
+              <img src="/img/sunlightright.png" class="card-img-top" alt="...">
+            </div>
             <div class="card-body">
-              <h5 class="card-title">Hope Channel Inc.</h5>
-              <p class="card-text">Contracter as a web specialist since 2023.</p>
-              <a href="https://hopetv.org/" target="_blank" class="btn btn-primary">Visit here</a>
+              <h5 class="card-title">Sunlight Right</h5>
+              <p class="card-text">Get today's estimated vitamin D levels you could get from the sun!</p>
+              <a href="https://sunlightright.org/" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-solid fa-link"></i></a>
             </div>
           </div>
         </div>
 
         <div class="col-sm">
           <div class="card mt-2">
-            <img src="/img/logo-transparent.png" class="card-img-top" alt="...">
+            <div class="ratio ratio-1x1">
+              <img src="/img/HC Logo.png" class="card-img-top" alt="...">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Hope Channel Inc.</h5>
+              <p class="card-text">Contracter as a web specialist since 2023 for Hope Channel International.</p>
+              <a href="https://hopechannelinternational.org/" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-solid fa-link"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="card mt-2">
+            <div class="ratio ratio-1x1">
+              <img src="/img/logo-transparent.png" class="card-img-top" alt="...">
+            </div>
             <div class="card-body">
               <h5 class="card-title">Caring Angels LLC</h5>
-              <p class="card-text">Web developer for a caregiving agency.</p>
-              <a href="https://caring-angels-llc.com/" target="_blank" class="btn btn-primary">Visit here</a>
+              <p class="card-text">Web developer for a caregiving agency providing service to the DMV area.</p>
+              <a href="https://caring-angels-llc.com/" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-solid fa-link"></i></a>
             </div>
           </div>
         </div>
@@ -113,11 +139,13 @@
         <div class="col-sm">
 
           <div class="card mt-2">
-            <img src="/img/robot.jpg" class="card-img-top" alt="...">
+            <div class="ratio ratio-1x1">
+              <img src="/img/robot.jpg" class="card-img-top" alt="...">
+            </div>
             <div class="card-body">
               <h5 class="card-title">Autoflusher Bot</h5>
               <p class="card-text">3D printed robot with Arduino hardware built to flush the toilet.</p>
-              <a href="https://youtu.be/WqopIXGkR5I?si=rA0KsFy9vCWZ8E_3" target="_blank" class="btn btn-primary">Watch video</a>
+              <a href="https://youtu.be/WqopIXGkR5I?si=rA0KsFy9vCWZ8E_3" target="_blank" class="btn btn-lg btn-primary"><i class="fa fa-youtube-play" id="youtube-icon"></a>
             </div>
           </div>
         </div>
